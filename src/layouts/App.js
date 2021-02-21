7334be4e13f0258d97fdcfdf34c55e8dca46d5af
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-02-21 19:20:56 +0100

    Page divided with flexbox

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "../styles/App.css";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./Header";
+import Navigation from "./Navigation";
+import Page from "./Page";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
+        <div className="app">
           <header>
             <Header />
           </header>
