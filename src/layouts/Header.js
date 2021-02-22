b09dbda4e87eb6cbe7ba63c1a4bf5e479b26c80d
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-02-22 14:40:04 +0100

    images downloaded  to  images folder

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,5 +1,14 @@
 import React from "react";
 
+import img1 from "../images/header1.jpg";
+import img2 from "../images/header2.jpg";
+import img3 from "../images/header3.jpg";
+import img4 from "../images/header4.jpg";
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      {" "}
+      <img src={img1} alt="Landscape" />{" "}
+    </header>
+  );
 }
