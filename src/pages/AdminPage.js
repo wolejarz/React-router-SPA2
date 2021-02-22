0caeb2dbe49280849a36be57426033f67d18eb0e
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-02-22 18:29:35 +0100

    changes at footer - info about current location

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
 const permission = false;
 
 const AdminPage = () => {
-    return (
-        <Route render={() => (
-            permission ? (<h3>Panel admina - dzień dobry</h3>) : (
-                <Redirect to="/login" />
-            )
-        )} />
-    );
-}
+  return (
+    <Route
+      render={() =>
+        permission ? <h3>Admin panel - welcome</h3> : <Redirect to="/login" />
+      }
+    />
+  );
+};
 
 export default AdminPage;
