6328195f913269ff6e998841df70d31227d0124a
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-02-22 18:28:23 +0100

    footer modified - info about current location

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,5 +1,57 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import "../styles/Footer.css";
+const Footer = () => {
+  return (
+    <div>
+      <h2>Footer</h2>
+      <Route
+        path="/"
+        exact
+        render={(props) => (
+          <p>
+            You are at <span>Home Page</span>
+          </p>
+        )}
+      />
 
-export default function Footer() {
-  return <div>Footer</div>;
-}
+      <Route
+        path="/:page"
+        exact
+        render={(props) => {
+          console.log(props);
+          return (
+            <>
+              <p>
+                You are at <span>{props.match.params.page}</span>
+              </p>
+              <p>
+                You are at URL: <span>{props.match.url}</span>
+              </p>
+            </>
+          );
+        }}
+      />
+
+      <Route
+        path="/:page/:idProduct"
+        exact
+        render={(props) => {
+          console.log(props);
+          return (
+            <>
+              <p>
+                You are at <span>{props.match.params.idProduct}</span>
+              </p>
+              <p>
+                You are at URL: <span>{props.match.url}</span>
+              </p>
+            </>
+          );
+        }}
+      />
+    </div>
+  );
+};
+
+export default Footer;
