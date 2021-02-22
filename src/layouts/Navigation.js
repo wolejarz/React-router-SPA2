a3fa10f1952ae8f378e2087591841c25d442f306
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-02-22 13:26:25 +0100

    navbar is ready

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Navigation.css";
 
+const list = [
+  { name: "Start", path: "/" },
+  { name: "Products", path: "/products" },
+  { name: "Contact", path: "/contact" },
+  { name: "Admin", path: "/admin" },
+];
 export default function Navigation() {
-  return <div>Navigation</div>;
+  const navlist = list.map((element) => (
+    <li key={element.name}>
+      <NavLink exact to={element.path}>
+        {element.name}
+      </NavLink>
+    </li>
+  ));
+  return (
+    <nav className="main">
+      <ul>{navlist}</ul>
+    </nav>
+  );
 }
